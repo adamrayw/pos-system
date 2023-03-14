32fd9b375253272365bbcb816a3550a26c0168d4
adamrayw <raywibowo68@gmail.com>
2023-03-14 11:59:17 +0700

    Menu component

---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,82 @@
+import Soto from '../../assets/soto.png'
+
+function Menu() {
+    return (
+        <div className='grid grid-cols-3 gap-4'>
+            <button className='p-4 bg-white items-center space-y-2 shadow-sm active:bg-gray-100'>
+                <div className="flex justify-center items-center">
+                    <img src={Soto} width={100} height={100} alt='menu' priority />
+                </div>
+
+                <div className="text-left">
+                    <h3 className='text-xl font-bold text-gray-800'>Soto</h3>
+                    <p className='text-lg font-medium text-orange-500'>
+                        Rp 10.000
+                    </p>
+                </div>
+            </button>
+            <button className='p-4 bg-white items-center space-y-2 shadow-sm active:bg-gray-100'>
+                <div className="flex justify-center items-center">
+                    <img src={Soto} width={100} height={100} alt='menu' priority />
+                </div>
+
+                <div className="text-left">
+                    <h3 className='text-xl font-bold text-gray-800'>Soto</h3>
+                    <p className='text-lg font-medium text-orange-500'>
+                        Rp 10.000
+                    </p>
+                </div>
+            </button>
+            <button className='p-4 bg-white items-center space-y-2 shadow-sm active:bg-gray-100'>
+                <div className="flex justify-center items-center">
+                    <img src={Soto} width={100} height={100} alt='menu' priority />
+                </div>
+
+                <div className="text-left">
+                    <h3 className='text-xl font-bold text-gray-800'>Soto</h3>
+                    <p className='text-lg font-medium text-orange-500'>
+                        Rp 10.000
+                    </p>
+                </div>
+            </button>
+            <button className='p-4 bg-white items-center space-y-2 shadow-sm active:bg-gray-100'>
+                <div className="flex justify-center items-center">
+                    <img src={Soto} width={100} height={100} alt='menu' priority />
+                </div>
+
+                <div className="text-left">
+                    <h3 className='text-xl font-bold text-gray-800'>Soto</h3>
+                    <p className='text-lg font-medium text-orange-500'>
+                        Rp 10.000
+                    </p>
+                </div>
+            </button>
+            <button className='p-4 bg-white items-center space-y-2 shadow-sm active:bg-gray-100'>
+                <div className="flex justify-center items-center">
+                    <img src={Soto} width={100} height={100} alt='menu' priority />
+                </div>
+
+                <div className="text-left">
+                    <h3 className='text-xl font-bold text-gray-800'>Soto</h3>
+                    <p className='text-lg font-medium text-orange-500'>
+                        Rp 10.000
+                    </p>
+                </div>
+            </button>
+            <button className='p-4 bg-white items-center space-y-2 shadow-sm active:bg-gray-100'>
+                <div className="flex justify-center items-center">
+                    <img src={Soto} width={100} height={100} alt='menu' priority />
+                </div>
+
+                <div className="text-left">
+                    <h3 className='text-xl font-bold text-gray-800'>Soto</h3>
+                    <p className='text-lg font-medium text-orange-500'>
+                        Rp 10.000
+                    </p>
+                </div>
+            </button>
+        </div>
+    )
+}
+
+export default Menu
