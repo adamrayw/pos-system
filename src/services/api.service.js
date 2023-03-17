c79fae2edef43443ceef0213ffa512ae703407fb
adamrayw <raywibowo68@gmail.com>
2023-03-17 22:40:04 +0700

    create useApiRequest method

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -1,3 +1,28 @@
-const api = 'http://localhost:8080/api/'
+const api = 'http://localhost:8181/api/'
+import axios from "axios"
+import { useEffect, useState } from "react"
 
-export default api
+const useApiRequest = url => {
+    const [data, setData] = useState([])
+    const [isLoaded, setIsLoaded] = useState(false)
+    const [error, setError] = useState(null)
+
+    useEffect(() => {
+        const fetchData = () => {
+            axios
+                .get(api + url)
+                .then(response => {
+                    setIsLoaded(true)
+                    setData(response.data)
+                })
+                .catch(error => {
+                    setError(error)
+                })
+        }
+        fetchData()
+    }, [url])
+
+    return { error, isLoaded, data }
+}
+
+export default useApiRequest
