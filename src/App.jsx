8728efabf6a612bf053c65c5201c96126e84654f
adamrayw <raywibowo68@gmail.com>
2023-04-25 22:26:25 +0700

    feature: protect all route, check the jwt token is expired or not

--- src/App.jsx
+++ src/App.jsx
@@ -9,25 +9,65 @@ import Menu from "./pages/Manage/Menu";
 import ManageMenu from "./pages/ManageMenu";
 import 'react-toastify/dist/ReactToastify.css';
 import Kategori from "./pages/Manage/Kategori";
+import Login from "./pages/Auth/Login/Login";
+import { useState } from "react";
+import Protected from "./components/Protected/Protected";
+import jwtDecode from "jwt-decode";
 
 function App() {
   const { pathname } = useLocation();
 
+  const token = localStorage.getItem("token");
+  const decodedToken = token ? jwtDecode(token) : null;
+
+  if (decodedToken && decodedToken.exp * 1000 < new Date().getTime()) {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    window.location.href = "/auth/login";
+    alert("Sesi anda telah berakhir, silahkan login kembali");
+  }
+
+
   return (
     <div className="App flex">
-      <Sidebar />
-      <div className="bg-gray-100 py-10 px-5 md:p-10 w-full max-h-screen overflow-auto">
+      <div className="bg-white">
+        {pathname === '/auth/login' ? <></> : <Sidebar />}
+      </div>
+      <div className={` ${pathname === '/auth/login' ? 'bg-white' : 'bg-gray-100'}  py-10 px-5 md:p-10 w-full max-h-screen overflow-auto`}>
         <Routes>
-          <Route path='/' element={<Index />} />
-          <Route path='laporan' element={<Laporan />} />
-          <Route path='about' element={<About />} />
+          <Route path='/' element={
+            <Protected>
+              <Index />
+            </Protected>
+          } />
+          <Route path='laporan' element={
+            <Protected>
+              <Laporan />
+            </Protected>
+          } />
+          <Route path='about' element={
+            <Protected>
+              <About />
+            </Protected>
+          } />
           <Route path='manage' >
-            <Route path='menu' element={<Menu />} />
-            <Route path='kategori' element={<Kategori />} />
+            <Route path='menu' element={
+              <Protected>
+                <Menu />
+              </Protected>
+            } />
+            <Route path='kategori' element={
+              <Protected>
+                <Kategori />
+              </Protected>
+            } />
+          </Route>
+          <Route path='auth'>
+            <Route path='login' element={<Login />} />
           </Route>
         </Routes>
       </div>
-      {pathname === '/laporan' ? <></> : <RightBar />}
+      {pathname === '/auth/login' ? <></> : <RightBar />}
       <ToastContainer />
     </div >
   )
