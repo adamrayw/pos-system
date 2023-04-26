0cec33e53e7b13a22df8b01d1dd0f469ac7b59e5
adamrayw <raywibowo68@gmail.com>
2023-04-26 20:32:02 +0700

    refactor: add alert message in login

--- src/pages/Auth/Login/Login.jsx
+++ src/pages/Auth/Login/Login.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 import posku from '../../../../public/assets/posku-logo.png';
 import { useApiLogin } from '../../../services/api.service';
+import AlertError from '../../../components/Alert/AlertError';
 
 const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [isLoading, setIsLoading] = useState(false);
+    const [errResponse, setErrResponse] = useState({});
 
     const handleUsernameChange = event => setUsername(event.target.value);
     const handlePasswordChange = event => setPassword(event.target.value);
@@ -16,10 +18,10 @@ const Login = () => {
         e.preventDefault()
 
         if (!email || !password) {
-            // setErrResponse({
-            //     status_text: "warning",
-            //     message: "Please fill all the fields!"
-            // })
+            setErrResponse({
+                status_text: "warning",
+                message: "Please fill all the fields!"
+            })
             return
         }
 
@@ -44,6 +46,10 @@ const Login = () => {
             setIsLoading(false)
         } catch (error) {
             setIsLoading(false)
+            setErrResponse({
+                status_text: "error",
+                message: "Something went wrong!"
+            })
             console.log("err: " + error)
         }
     };
@@ -52,6 +58,9 @@ const Login = () => {
         <div className="flex flex-col w-full max-w-md mx-auto mt-8 px-4 py-8 bg-white rounded-lg shadow sm:px-6 md:px-8 lg:px-10">
             <div className="self-center mb-6 text-xl font-light text-gray-500 sm:text-2xl">
                 <img src={posku} alt="logo" className='w-auto h-10' />
+            </div>
+            <div>
+                <AlertError message={errResponse} />
             </div>
             <div className="mt-8">
                 <form action="#" autoComplete="off" onSubmit={handleLogin}>
@@ -79,7 +88,7 @@ const Login = () => {
                     </div>
                     <div className="flex items-center mb-6 -mt-4">
                         <div className="flex ml-auto">
-                            <a href="#" className="inline-flex text-xs font-thin text-gray-500 sm:text-sm hover:text-gray-700">
+                            <a href="#" className="inline-flex text-xs font-normal text-gray-500 sm:text-sm hover:text-gray-700">
                                 Lupa Password?
                             </a>
                         </div>
@@ -99,7 +108,7 @@ const Login = () => {
                 </form>
             </div>
             <div className="flex items-center justify-center mt-6">
-                <a href="#" target="_blank" className="inline-flex items-center text-xs font-thin text-center text-gray-500 hover:text-gray-700 ">
+                <a href="https://posku-main.vercel.app/" target="_blank" className="inline-flex items-center tracking-wide text-sm font-normal text-center text-gray-500 hover:text-gray-700 ">
                     <span className="ml-2">
                         Belum berlangganan?
                     </span>
