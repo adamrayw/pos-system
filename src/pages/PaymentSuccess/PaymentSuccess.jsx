f7cc1047f506b224c6c0a57a2049e98f8a352e0a
adamrayw <raywibowo68@gmail.com>
2023-05-22 12:22:11 +0700

    Payment Success Page

---
+++ src/pages/PaymentSuccess/PaymentSuccess.jsx
@@ -0,0 +1,29 @@
+import PaymentSuccessImg from '../../assets/payment_success.svg'
+
+function PaymentSuccess() {
+    return (
+        <div className="md:px-12 px-4 mt-10 h-screen">
+            <div className="flex items-center justify-center">
+                <div className='text-center space-y-2'>
+                    <div className="card w-fit bg-white text-slate-600">
+                        <div className="card-body items-center text-center py-10 px-8 rounded shadow-sm space-y-2">
+                            <img src={PaymentSuccessImg} alt="payment_success_img" className='w-40 h-40 mx-auto' />
+                            <h1 className="font-bold md:text-4xl text-2xl">Pembayaran Berhasil!</h1>
+                            <p className="text-gray-500 font-normal md:text-sm text-sm">
+                                Silahkan cek status pembayaran anda di dashboard atau di laporan.
+                            </p>
+                            <div className='card-actions flex items-center justify-center flex-col-reverse md:flex-row md:space-x-2 px-4 pt-4 font-semibold'>
+                                <a href='https://posku.vercel.app/laporan' className='w-full px-4 py-3 rounded bg-gray-500 hover:bg-gray-600 transition text-white text-xs md:mb-0 mb-4 active:bg-gray-700'>Pergi ke Laporan (APP)</a>
+                                <a href='https://posku-main.vercel.app/dashboard' className='w-full px-4 py-3 rounded bg-orange-500 hover:bg-orange-600 active:bg-orange-700 transition border-0 text-white text-xs '>
+                                    Pergi ke Dashboard (WEB)
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default PaymentSuccess
