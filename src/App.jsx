cc1b08e34865462efcaab1d30e27da1dc5ddfa15
adamrayw <raywibowo68@gmail.com>
2023-05-30 10:23:24 +0700

    feat: add route for landing page and not found page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import RightBar from "./components/RightBar/RightBar";
 import Sidebar from "./components/Sidebar/Sidebar";
 import About from "./pages/About";
 import Index from "./pages/Index";
+import Home from "./pages/LandingPage/Home/Home.jsx";
+import Dashboard from "./pages/LandingPage/Dashboard/Dashboard.jsx";
+import SuccessCheckout from "./pages/LandingPage/SuccessCheckout/SuccessCheckout.jsx";
 import Laporan from "./pages/Laporan";
 import Menu from "./pages/Manage/Menu";
 import PaymentSuccess from "./pages/PaymentSuccess/PaymentSuccess";
@@ -13,6 +16,8 @@ import Login from "./pages/Auth/Login/Login";
 import { useState } from "react";
 import Protected from "./components/Protected/Protected";
 import jwtDecode from "jwt-decode";
+import NotFound from "./pages/NotFound/NotFound";
+import MissingRoute from "./components/MissingRoute/MissingRoute";
 
 function App() {
   const { pathname } = useLocation();
@@ -29,38 +34,45 @@ function App() {
 
 
   return (
-    <div className="App flex">
+    <div className="App flex relative">
       <div className="bg-gray-50">
-        {pathname === '/auth/login' || pathname === '/payment-success' ? <></> : <Sidebar />}
+        {pathname === '/auth/login' || pathname === '/payment-success' || pathname === '/' || pathname === '/dashboard' | pathname === '/success-checkout' || pathname === "/404" ? <></> : <Sidebar />}
       </div>
-      <div className={` ${pathname === '/auth/login' ? 'bg-gray-50' : 'bg-gray-100'}  py-10 md:p-10 w-full h-screen overflow-auto`}>
+      <div className={` ${pathname === '/auth/login' || pathname === "/" || pathname === '/dashboard' ? 'bg-white w-full h-screen overflow-auto' : 'bg-gray-100 py-10 md:p-10 w-full h-screen overflow-auto'}  `}>
         <Routes>
-          <Route path='/' element={
-            <Protected>
-              <Index />
-            </Protected>
-          } />
-          <Route path='laporan' element={
-            <Protected>
-              <Laporan />
-            </Protected>
-          } />
-          <Route path='about' element={
-            <Protected>
-              <About />
-            </Protected>
-          } />
-          <Route path='manage' >
-            <Route path='menu' element={
+          {/* Landing Page Routes */}
+          <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/success-checkout" element={<SuccessCheckout />} />
+          {/* App Routes */}
+          <Route path="app">
+            <Route index element={
               <Protected>
-                <Menu />
+                <Index />
               </Protected>
             } />
-            <Route path='kategori' element={
+            <Route path='laporan' element={
               <Protected>
-                <Kategori />
+                <Laporan />
               </Protected>
             } />
+            <Route path='about' element={
+              <Protected>
+                <About />
+              </Protected>
+            } />
+            <Route path='manage' >
+              <Route path='menu' element={
+                <Protected>
+                  <Menu />
+                </Protected>
+              } />
+              <Route path='kategori' element={
+                <Protected>
+                  <Kategori />
+                </Protected>
+              } />
+            </Route>
           </Route>
           <Route path='auth'>
             <Route path='login' element={<Login />} />
@@ -68,9 +80,15 @@ function App() {
           <Route path='payment-success' element={
             <PaymentSuccess />
           } />
+          <Route path="*" element={
+            <MissingRoute>
+              <NotFound />
+            </MissingRoute>
+          } />
+          <Route path="404" element={<NotFound />} />
         </Routes>
       </div>
-      {pathname === '/auth/login' || pathname === '/payment-success' ? <></> : <RightBar />}
+      {pathname === '/auth/login' || pathname === '/payment-success' || pathname === '/' || pathname === '/dashboard' || pathname === '/success-checkout' || pathname === "/404" ? <></> : <RightBar />}
       <ToastContainer />
     </div >
   )
