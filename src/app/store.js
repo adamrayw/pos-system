71f97a5b6c9a14d92f86c1e2cf2500f35e5eb66f
adamrayw <raywibowo68@gmail.com>
2023-05-30 10:23:53 +0700

    feat: add modal reducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import menuReducer from '../features/menuSlice/menuSlice'
+import modalReducer from '../features/modal/modalSlice'
 
 export const store = configureStore({
     reducer: {
-        menu: menuReducer
+        menu: menuReducer,
+        modal: modalReducer
     }
 })
