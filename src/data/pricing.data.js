a950cf0588cd41c6059093067a7a609e954adae5
adamrayw <raywibowo68@gmail.com>
2023-05-30 10:25:10 +0700

    refactor: remove datadummy and add data pricing

---
+++ src/data/pricing.data.js
@@ -0,0 +1,22 @@
+const data = [
+    {
+        id: 1,
+        nama_paket: "Basic",
+        duration: "1 Bulan",
+        harga: 100000,
+    },
+    {
+        id: 2,
+        nama_paket: "Plus",
+        duration: "1 Tahun",
+        harga: 500000,
+    },
+    {
+        id: 3,
+        nama_paket: "Pro",
+        duration: "3 Bulan",
+        harga: 200000,
+    },
+];
+
+export default data;
