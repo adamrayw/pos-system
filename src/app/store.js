0dc46b9426cdaf9515bb2d759faf74e7cbe96be6
adamrayw <raywibowo68@gmail.com>
2023-07-08 11:54:50 +0700

    feature: add middleware in react-redux

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import menuReducer from '../features/menuSlice/menuSlice'
 import modalReducer from '../features/modal/modalSlice'
+import logger from 'redux-logger'
 
 export const store = configureStore({
     reducer: {
         menu: menuReducer,
         modal: modalReducer
-    }
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 })
